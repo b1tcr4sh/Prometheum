957e74b14abb6536ff7d9325047ffd21f1809a35
Ethan Ford <ethan.ford06@gmail.com>
2022-01-21 23:05:40 -0600

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Prometheum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
