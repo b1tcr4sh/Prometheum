2a27ae6b34b32e5f35c04ac90d331d277e5411e5
VividUwU <ethan.ford06@gmail.com>
2022-01-30 16:22:54 -0600

    Rearranged file structure

--- DiscordBot.cs
+++ DiscordBot.cs
@@ -7,41 +7,43 @@ using DSharpPlus.EventArgs;
 using DSharpPlus.Exceptions;
 using Prometheum.Config;
 
-public class DiscordBot {
-    public String token;
-    private DiscordClient client;
-    public async Task ConnectAsync() {
+namespace Prometheum {
+    public class DiscordBot {
+        public String token;
+        private DiscordClient client;
+        public async Task ConnectAsync() {
 
-        Config config = ConfigInit();
+            Config config = ConfigInit();
 
-        if (config.Token == null) return;
+            if (config.Token == null) return;
 
-        client = new DiscordClient(new DiscordConfiguration {
-            AutoReconnect = true,
-            MinimumLogLevel = Microsoft.Extensions.Logging.LogLevel.Debug,
-            Token = token,
-            TokenType = TokenType.Bot
-        });
+            client = new DiscordClient(new DiscordConfiguration {
+                AutoReconnect = true,
+                MinimumLogLevel = Microsoft.Extensions.Logging.LogLevel.Debug,
+                Token = token,
+                TokenType = TokenType.Bot
+            });
 
-        await client.ConnectAsync();
+            await client.ConnectAsync();
 
-        client.GuildDownloadCompleted += ListServersAndChannels;
+            client.GuildDownloadCompleted += ListServersAndChannels;
 
-        await Task.Delay(-1);
-    }
-    private Config ConfigInit() {
-        ConfigManager manager = new ConfigManager("./config.json");
-        token = manager.Config.Token;
+            await Task.Delay(-1);
+        }
+        private Config ConfigInit() {
+            ConfigManager manager = new ConfigManager("./config.json");
+            token = manager.Config.Token;
 
-        return manager.Config;
-    }
-    public async Task ListServersAndChannels(object sender, GuildDownloadCompletedEventArgs e) {
-        foreach (KeyValuePair<ulong, DiscordGuild> serverKeyPair in client.Guilds) {
-            Console.WriteLine("Server {0}:", serverKeyPair.Value.Name);
+            return manager.Config;
+        }
+        public async Task ListServersAndChannels(object sender, GuildDownloadCompletedEventArgs e) {
+            foreach (KeyValuePair<ulong, DiscordGuild> serverKeyPair in client.Guilds) {
+                Console.WriteLine("Server {0}:", serverKeyPair.Value.Name);
 
-            IReadOnlyList<DiscordChannel> channels = await serverKeyPair.Value.GetChannelsAsync();
-            foreach (DiscordChannel channel in channels) {
-                Console.WriteLine("     " + channel.Name);
+                IReadOnlyList<DiscordChannel> channels = await serverKeyPair.Value.GetChannelsAsync();
+                foreach (DiscordChannel channel in channels) {
+                    Console.WriteLine("     " + channel.Name);
+                }
             }
         }
     }
