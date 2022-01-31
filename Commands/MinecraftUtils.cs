3ed4773b9c249007aa192cb23e1e65c04870eac7
VividUwU <ethan.ford06@gmail.com>
2022-01-30 18:00:49 -0600

    Added TODO reminders

--- Commands/MinecraftUtils.cs
+++ Commands/MinecraftUtils.cs
@@ -12,20 +12,22 @@ using Prometheum;
 
 namespace Prometheum.Commands {
     class MinecraftUtils : BaseCommandModule {
+        // TODO: Server register command that will register a server address and store it in DB as this Guild's MC server.  Will be a class member that is used in all minecraft server commands.
+
         [Command("status")]
         public async Task Status(CommandContext context, String ServerAddress) {
-            //  RCON rconClient = new RCON(IPAddress.Parse(ServerAddress), 25575, RconPassword);
+            // TODO: Check if address is a url or ip address, and fetch the address of the url if needed before passing in to query.
 
-             MinecraftQueryInfo serverStatus = await ServerQuery.Info(IPAddress.Parse(ServerAddress), 25575, ServerQuery.ServerType.Minecraft) as MinecraftQueryInfo;
+            MinecraftQueryInfo serverStatus = await ServerQuery.Info(IPAddress.Parse(ServerAddress), 25575, ServerQuery.ServerType.Minecraft) as MinecraftQueryInfo;
 
-             DiscordEmbedBuilder embedBuilder = new DiscordEmbedBuilder();
-             embedBuilder.Color = DiscordColor.Green;
-             embedBuilder.Title = $"{serverStatus.HostIp} Status";
-             embedBuilder.Description = $"{serverStatus.MessageOfTheDay}";
-             embedBuilder.AddField("Players Online:", serverStatus.NumPlayers);
-             DiscordEmbed embed = embedBuilder.Build();
+            DiscordEmbedBuilder embedBuilder = new DiscordEmbedBuilder();
+            embedBuilder.Color = DiscordColor.Green;
+            embedBuilder.Title = $"{serverStatus.HostIp} Status";
+            embedBuilder.Description = $"{serverStatus.MessageOfTheDay}";
+            embedBuilder.AddField("Players Online:", serverStatus.NumPlayers);
+            DiscordEmbed embed = embedBuilder.Build();
 
-             await context.Channel.SendMessageAsync(embed);
+            await context.Channel.SendMessageAsync(embed);
         }
     }
 }
