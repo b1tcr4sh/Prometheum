5ee8d9493946d0af467db84ef202880143c02c9b
Ethan Ford <ethan.ford06@gmail.com>
2022-03-02 15:25:27 -0600

    Added Minecraft server DB models

---
+++ Database/CollectionModels/MinecraftDiscordUserPair.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace Prometheum.MC {
+    public class MinecraftDiscordUserPair {
+        public string MinecraftUsername { get; set; }
+        public string DiscordUsername { get; set; }
+    }
+}
---
+++ Database/CollectionModels/MinecraftServer.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Generic;
+
+namespace Prometheum.MC {
+    public class MinecraftServer {
+        public string Address { get; set; }
+        public List<MinecraftDiscordUserPair> Users { get; set; }
+
+    }
+}
