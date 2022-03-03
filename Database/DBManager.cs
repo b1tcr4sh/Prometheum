849d41f035233971c50abc3e18baf743c6b33354
Ethan Ford <ethan.ford06@gmail.com>
2022-03-03 13:10:33 -0600

    Added MinecraftServer specfic DB methods

--- Database/DBManager.cs
+++ Database/DBManager.cs
@@ -1,9 +1,11 @@
 using System;
 using System.Threading.Tasks;
 using System.Collections.Generic;
+using System.Linq.Expressions;
 using MongoDB.Bson;
 using MongoDB.Driver;
 using MongoDB.Bson.Serialization;
+using Prometheum.Database.MC;
 
 namespace Prometheum.Database {
     public static class DBManager {
@@ -23,9 +25,22 @@ namespace Prometheum.Database {
             IMongoCollection<T> collection = Database.GetCollection<T>(collectionName);
             await collection.InsertOneAsync(objectToUpload);
         }
-        // public async Task RemoveDocument<T>(string collectionName) {
-        //     IMongoCollection<T> collection = Database.GetCollection<T>(collectionName);
-        // }
+        public static void UpdateMinecraftServerDocument<MinecraftServer>(Expression<Func<MinecraftServer, bool>> filter, string serverAddress) {
+            IMongoCollection<MinecraftServer> collection = Database.GetCollection<MinecraftServer>("MinecraftServers");
+            UpdateDefinition<MinecraftServer> update = Builders<MinecraftServer>.Update.Set("Address", serverAddress);
+
+            collection.UpdateOne<MinecraftServer>(filter, update);
+        }
+        public static MinecraftServer GetMinecraftServerDocument(ulong serverId) {
+            IMongoCollection<MinecraftServer> collection = Database.GetCollection<MinecraftServer>("MinecraftServers");
+            IFindFluent<MinecraftServer, MinecraftServer> finder = collection.Find<MinecraftServer>(document => document.ServerId == serverId);
+            
+            if (finder.CountDocuments() > 1) {
+                return null;
+            } else {
+                return finder.First<MinecraftServer>();
+            }
+        }
         public static async Task<List<string>> GetCollectionNames() {
             IAsyncCursor<string> collections = await Database.ListCollectionNamesAsync();
 
